3f2697eece0ef948c4b07095fb6e60909048b28a
Ricky Irawan <rickyirawan9@gmail.com>
2025-03-12 12:57:12 +0700

    purwarupa popup sudah disiapkan + vercel.json

--- src/Components/filmComponents/TheLittleMermaid.jsx
+++ src/Components/filmComponents/TheLittleMermaid.jsx
@@ -1,11 +1,21 @@
 const theLittleMermaidPicture = "/assets/assetgambar/assetgambarberanda/filmnya/thelittlemermaid.png";
-
+import { useState } from "react";
 
 import "../filmComponents/filmobjectcss.css"
+
 function TheLittleMermaid(){
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
         <>
-        <div className="filmobject" style={{ backgroundImage:`url(${theLittleMermaidPicture})`}}></div>
+        <div className="filmobject"  onClick={() => setIsOpen(true)} style={{ backgroundImage:`url(${theLittleMermaidPicture})`} }>
+            {isOpen && (
+                <div className="filmpopup">
+                    Muncullah Film Apalah Ini
+                    <button onClick={()=>setIsOpen(false)}>X</button>
+                </div>
+            )}
+        </div>
         </>
     )
 }
